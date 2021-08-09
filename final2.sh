314c2669cfdd24ae4a92a10eb8b173b1c475885a
raspberrypisig <raspberrypisig@gmail.com>
2021-08-09 23:28:45 +1000

    Add files via upload

--- final2.sh
+++ final2.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 set -x
-setterm --cursor off
+echo -e '\033[?25l'
 #setsid bash -c 'sudo chvt 1; cd /home/pi/Desktop ; exec ./run.sh <> /dev/tty1 >&0 2>&1'
-setterm --cursor off
 /home/pi/Desktop/run.sh
 #read  < /dev/tty
 sudo pkill micropython
