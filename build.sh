2ebed4679a73ec4301810fcaa55ce47dfc6128f7
raspberrypisig <raspberrypisig@gmail.com>
2021-08-14 01:05:42 +1000

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,11 +4,11 @@ sudo apt update
 sudo apt install -y build-essential libreadline-dev libffi-dev git pkg-config libsdl2-2.0-0 libsdl2-dev
 git clone --recurse-submodules https://github.com/lvgl/lv_micropython
 cd lv_micropython/
-sed -ir 's/LV_FONT_MONTSERRAT_48    1/LV_FONT_MONTSERRAT_48    0/' lib/lv_bindings/lv_conf.h
-sed -ir 's/LV_FONT_MONTSERRAT_32    1/LV_FONT_MONTSERRAT_32    0/' lib/lv_bindings/lv_conf.h
+sed -ir 's/LV_FONT_MONTSERRAT_48    0/LV_FONT_MONTSERRAT_48    1/' lib/lv_bindings/lv_conf.h
+sed -ir 's/LV_FONT_MONTSERRAT_32    0/LV_FONT_MONTSERRAT_32    1/' lib/lv_bindings/lv_conf.h
 make -C mpy-cross
 make -C ports/unix
-cd Desktop
+cd /home/pi/Desktop
 wget https://github.com/raspberrypisig/lvgl-demo/raw/main/clock.py
 wget https://github.com/raspberrypisig/lvgl-demo/raw/main/final3.sh
 wget https://github.com/raspberrypisig/lvgl-demo/raw/main/final2.sh
