ae46a7d94a0b9eeb3a6e5c3291def88d0f05fe49
raspberrypisig <raspberrypisig@gmail.com>
2021-08-16 13:50:18 +1000

    Add files via upload

--- dist/final3.sh
+++ dist/final3.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
+#who|grep tty1
+#while [ $? -ne 0  ];
+#do
+#true
+#done
+sleep 3
 cd /home/pi/Desktop
 sudo openvt -s ./final2.sh
--- dist/run.sh
+++ dist/run.sh
@@ -1,3 +1,2 @@
 #!/usr/bin/env bash
-setterm --cursor off
 MICROPYPATH=/home/pi/lv_micropython/lib/lv_bindings/lib micropython /home/pi/Desktop/clock.py
